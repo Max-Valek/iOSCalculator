2c3f304dc86813a9342a04aa6d148c436f05a592
Max Valek <valek.maxwell@gmail.com>
2023-08-01 13:08:54 -0500

    button text

--- Calculator/Models/ButtonType.swift
+++ Calculator/Models/ButtonType.swift
@@ -10,10 +10,13 @@ import Foundation
 /*
  ButtonType enum
  
- Numeric buttons encapsulated in the Digit enum
- Numberic operations encapsulated in ArithmeticOperation enum
+ Numeric buttons encapsulated in the Digit enum.
+ Numberic operations encapsulated in ArithmeticOperation enum.
+ 
+ Hashable: for iteration
+ CustomStringConvertible: button text
  */
-enum ButtonType {
+enum ButtonType: Hashable, CustomStringConvertible {
     case digit(_ digit: Digit)
     case operation(_ operation: ArithmeticOperation)
     case negative
@@ -22,4 +25,25 @@ enum ButtonType {
     case equals
     case allClear
     case clear
+    
+    var description: String {
+        switch self {
+        case .digit(let digit):
+            return digit.description
+        case .operation(let operation):
+            return operation.description
+        case .negative:
+            return "±"
+        case .percent:
+            return "%"
+        case .decimal:
+            return "."
+        case .equals:
+            return "="
+        case .allClear:
+            return "AC"
+        case .clear:
+            return "C"
+        }
+    }
 }
